f406949160914bd4ea0962f62d1f8866f70a4e1e
Rubbedikatz <bonzoxxl@web.de>
2019-07-21 22:06:34 +0200

    converted to d3.v4, first slider version

--- server/main.js
+++ server/main.js
@@ -4,7 +4,7 @@ var width = 960,
     geoPath;
 
 // insert svg element as canvas
-var svg = d3.select("body")
+var svg = d3.select("body") 
   .append("svg")
     .attr("width", width)
     .attr("height", height);
@@ -20,25 +20,54 @@ var div = d3.select("body").append("div")
     .style("opacity", 0);
 
 
-queue()
+d3.queue()
 	.defer(d3.json, "./data/bundeslander.json")
-	.defer(d3.json, "./data/cities.json")
+	.defer(d3.json, "./data/cities_new.json")
   .awaitAll(render_map);
   
 
 // insert states data
 function render_map(error, result_data) {
-  var bundeslander = result_data[0],
-      stations = result_data[1]
 
-  var bounds = d3.geo.bounds(bundeslander),
+  var mydiv = d3.select("body")
+  .append("div")
+  .attr("style", "text-align:center");
+
+  mydiv.append("input")
+      .attr("type", "range")
+      .attr("min", 0)
+      .attr("max", 1)
+      .attr("step", "1")
+      .attr("id", "year")
+      .on("input", function input() {
+        sliderUpdate(this, result_data[1]);
+      });
+
+  // get max and min delay of all trips to calculate color scale domain
+  var delays = [];
+  result_data[1].forEach(function (month, index) {
+    for (var station in month.features) {
+      delays.push(month.features[station]["properties"]["mean_delay"]);
+    }
+  });
+  var delay_max = Math.max(...delays),
+      delay_min = Math.min(...delays),
+      delay_mid = (delay_max+delay_min)/2,
+      myColor = d3.scaleLinear()
+                        .domain([delay_min, delay_mid, delay_max])
+                        .range(["green", "yellow", "red"]);
+  
+  var bundeslander = result_data[0],
+      stations = result_data[1][0]
+
+  var bounds = d3.geoBounds(bundeslander),
     bottomLeft = bounds[0],
     topRight = bounds[1],
     rotLong = -(topRight[0]+bottomLeft[0])/2;
     center = [(topRight[0]+bottomLeft[0])/2+rotLong, (topRight[1]+bottomLeft[1])/2],
     
     // first projection to get outer bounds and scaleFator
-    first_projection = d3.geo.albers()
+    first_projection = d3.geoAlbers()
       .parallels([bottomLeft[1],topRight[1]])
       .rotate([rotLong,0,0])
       .translate([width/2,height/2])
@@ -49,7 +78,7 @@ function render_map(error, result_data) {
     scaleFactor = 1.00*Math.min(width/(topRightPx[0]-bottomLeftPx[0]), height/(-topRightPx[1]+bottomLeftPx[1])),
     
     // second projection for plotting states and stations
-    second_projection = d3.geo.albers()
+    second_projection = d3.geoAlbers()
       .parallels([bottomLeft[1],topRight[1]])
       .rotate([rotLong,0,0])
       .translate([width/2,height/2])
@@ -57,7 +86,7 @@ function render_map(error, result_data) {
       .center(center);
 
   // create path object from projection to translate GeoJSON 
-  geoPath = d3.geo.path().projection(second_projection);
+  geoPath = d3.geoPath().projection(second_projection);
 
   // insert and render states
   g.selectAll("path.feature")
@@ -66,21 +95,7 @@ function render_map(error, result_data) {
       .append("path")
         .attr("class", "feature")
         .attr("d", geoPath)
-
-  // insert station data
         
-  // get max and min delay to calculate color scale domain
-  var delays = [];
-  for (var station in stations.features) {
-    delays.push(stations.features[station]["properties"]["mean_delay"]);
-  };
-  var delay_max = Math.max(...delays),
-      delay_min = Math.min(...delays),
-      delay_mid = (delay_max+delay_min)/2,
-      myColor = d3.scale.linear()
-                        .domain([delay_min, delay_mid, delay_max])
-                        .range(["green", "yellow", "red"]);
-  
   // insert and render stations
   g.selectAll("circle.feature")
       .data(stations.features)
@@ -91,7 +106,7 @@ function render_map(error, result_data) {
         .attr("cy", function(d){ return second_projection(d.geometry.coordinates)[1]; } )
         .attr("r", function(d){ return d.properties.trips/500; })
         .attr("fill", function(d){return myColor(d.properties.mean_delay) })
-        // tooltips
+        // add tooltips on mouseover
         .on("mouseover", function(d) {		
           div.transition()		
               .duration(200)		
@@ -105,44 +120,62 @@ function render_map(error, result_data) {
               .duration(500)		
               .style("opacity", 0);
           })
-        // event for zoom
+        // event for clicking on a station
         .on("click", clickStation);
+
+
+    function sliderUpdate(slider, data){
+      // update size and color of circles based on the slider position
+      g.selectAll("circle.feature")
+        .data(data[slider.value].features)
+        .transition(700)
+        .attr("r", function(d){ return d.properties.trips/500; })
+        .attr("fill", function(d){return myColor(d.properties.mean_delay) })
+        // updating data loses focus, so we remove the black border...
+        .style("stroke", "white");
+      g.transition()
+        // ...and zoom back out to default
+        .duration(700)
+        .attr("transform", "translate("+ (width/2) +","+ (height/2) +")scale(1)translate("+ (-width/2) +","+ (-height/2) +")")
+        .style("stroke-width", 1.75 +"px");
+    };
+
+    // this is run when a station is clicked
+    function clickStation(d) {
+      // set default non-zoom values
+      var x = width/2,
+          y = height/2,
+          k = 1;
+
+      // if the clicked station hasn't been in focus before click
+      if ((focused === null) || !(focused === d)) {
+        // set point to zoom in on
+        var centroid = geoPath.centroid(d),
+            x = +centroid[0],
+            y = +centroid[1],
+            k = 3; // zoom factor
+        focused = d;
+        // reset all circles to white borders, then paint focused one black
+        g.selectAll("circle").style("stroke", "white");
+        d3.select(this).style("stroke", "black");
+      } 
+      // if the clicked station was already in focus before click
+      else {
+        // leave zoom values as default -> zoom out
+        focused = null;
+        d3.select(this).style("stroke", "white");
       };
+
+      // change pixel size of states
+      // when k = 1 all sizes reset back to default
+      g.transition()
+        .duration(700)
+        .attr("transform", "translate("+ (width/2) +","+ (height/2) +")scale("+ k +")translate("+ (-x) +","+ (-y) +")")
+        .style("stroke-width", 1.75/k +"px")
+        // shrink circle size even more to make them more distinguishable
+        // when k=1 needs to match the initial number for radius set above
+        .selectAll("circle")
+          .attr("r", function(d){ return d.properties.trips/(300*k+200); });
+      };
+  };
       
-// this is run when a station is clicked
-function clickStation(d) {
-  // set default non-zoom values
-  var x = width/2,
-      y = height/2,
-      k = 1;
-
-  // if the clicked station hasn't been in focus before click
-  if ((focused === null) || !(focused === d)) {
-    // set point to zoom in on
-    var centroid = geoPath.centroid(d),
-        x = +centroid[0],
-        y = +centroid[1],
-        k = 3; // zoom factor
-    focused = d;
-    // reset all circles to white borders, then paint focused one black
-    g.selectAll("circle").style("stroke", "white");
-    d3.select(this).style("stroke", "black");
-  } 
-  // if the clicked station was already in focus before click
-  else {
-    // leave zoom values as default -> zoom out
-    focused = null;
-    d3.select(this).style("stroke", "white");
-  };
-
-  // change pixel size of states
-  // when k = 1 all sizes reset back to default
-  g.transition()
-    .duration(700)
-    .attr("transform", "translate("+ (width/2) +","+ (height/2) +")scale("+ k +")translate("+ (-x) +","+ (-y) +")")
-    .style("stroke-width", 1.75/k +"px")
-    // shrink circle size even more to make them more distinguishable
-    // when k=1 needs to match the initial number for radius set above
-    .selectAll("circle")
-      .attr("r", function(d){ return d.properties.trips/(300*k+200); });
-  };
